e85879f28930fd0b6aa37d6e1b51adb25f0fb862
lennon-jesus <161793432+lennon-jesus@users.noreply.github.com>
2025-05-04 13:02:51 -0300

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img id="nav" alt="Hotel Winner" src="winner.png" height="70"></a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Gerenciamento</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Relatórios</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Atendimento</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">
+            <span class="material-symbols-outlined">logout</span>
+            </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
